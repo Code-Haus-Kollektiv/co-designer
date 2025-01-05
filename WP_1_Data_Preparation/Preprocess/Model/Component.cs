071a3b660c436fcecca8576a7817df10ec97ab6d
Simon Lut <simonspammen@gmail.com>
2025-01-05 17:26:58 +0100

    feat:add document Model and refine Parameter model.

--- WP_1_Data_Preparation/Preprocess/Model/Component.cs
+++ WP_1_Data_Preparation/Preprocess/Model/Component.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Preprocess.Model
@@ -9,20 +10,46 @@ namespace Preprocess.Model
         public string InstanceId { get; set; }
         public string Name { get; set; }
         public string Nickname { get; set; }
+        public Type Type { get; set; }
+        public List<string> Keywords { get; set; }
         public string Description { get; set; }
+        public string InstanceDescription { get; set; }
         public string Category { get; set; }
         public string SubCategory { get; set; }
+        public bool IsObsolete { get; set; }
 
         // Parameters
         public List<Parameter> Parameters { get; set; }
 
         // State
-        public bool IsEnabled { get; set; }
+        public bool IsLocked { get; set; }
         public bool IsHidden { get; set; }
-        public (float X, float Y) Position { get; set; }
+        public Pivot Pivot { get; set; }
+        public string Message { get; set; }
+
 
         //Document
-        public string LibraryId { get; set; }
+        public Plugin Plugin { get; set; }
         public string DocumentInstanceId { get; set; }
+    }
+
+    public class Plugin
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Pivot
+    {
+        public Pivot(float X, float Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
     }
 }
